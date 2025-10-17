03bbe70cded1c14188a123c425e61e3891bdc42c
pangil5634 <oksk6681@naver.com>
2025-10-17 21:56:44 +0900

    feat : Convert string to an Integer / Calculate the sum

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
     const INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
     let arr = []; // 계산을 위한 배열 선언
+    let result = 0; // 결과를 위한 변수 선언
+    
     // 커스텀 구분자 존재 여부 확인에 따른 분기
     if (INPUT.slice(0, 2) === "//") {
       // 분기1 : 커스텀 구분자 존재 O
@@ -27,6 +29,9 @@ class App {
       // 콜론(:)으로 파싱 문자열을 분리
       temp.map(e => e.split(":").map(e => arr.push(e)));
     }
+
+    // 문자열에서 정수형으로 변환 및 합계 계산
+    arr.map(e => result += Number(e));
   }
 }
 
